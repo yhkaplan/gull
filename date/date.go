3c8a8294b6549c9dd6ce35e8abc332fa13368c57
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2018-09-27 17:52:44 +0900

    Get end of day

--- date/date.go
+++ date/date.go
@@ -16,3 +16,7 @@ func Parse(dateStr string) (time.Time, error) {
 
 	return t, nil
 }
+
+func EndOfDay(date time.Time) time.Time {
+	return time.Date(date.Year(), date.Month(), date.Day(), 23, 59, 59, int(time.Second-time.Nanosecond), timeZone)
+}
