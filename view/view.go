46671c01eba38a0acc2ebda152927a93a5f88d71
yhkaplan <yhkaplan@gmail.com>
2018-09-27 16:48:37 +0900

    More progress on View interface

--- view/view.go
+++ view/view.go
@@ -1,6 +1,10 @@
 package view
 
-import "github.com/jroimartin/gocui"
+import (
+	"log"
+
+	"github.com/jroimartin/gocui"
+)
 
 var g gocui.Gui
 var m gocui.Manager
@@ -12,14 +16,43 @@ func (manager) Layout(*gocui.Gui) error {
 	panic("not implemented")
 }
 
-func SetupView() error {
+// TODO: move this elsewhere
+type View struct {
+	g *gocui.Gui
+}
+
+func (v *View) size() (int, int) {
+	return v.g.size
+}
+
+func (v *View) layout(g *gocui.Gui) error {
+	maxX, maxY := v.size()
+
+	v.setCategoryPane(g)
+	v.setListPane(g)
+}
+
+// Setup left side category window
+func (v *View) setCategoryPane(g *gocui.Gui) error {
+	if
+}
+
+// Setup right side list window
+func (v *View) setListPane(g *gocui.Gui) error {
+
+}
+
+func (v *View) Run() {
 	g, err := gocui.NewGui(gocui.Output256)
 	if err != nil {
-		return err
+		log.Fatalf("NewGui: %v", err)
 	}
 	defer g.Close()
+	v.g = g
 
+	g.defaultSettings()
 	// Set manager and bindings
+	g.SetManagerFunc(v.layout)
 
 	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
 		return err
@@ -30,7 +63,8 @@ func SetupView() error {
 	return nil
 }
 
-func guiSettings(g *gocui.Gui) {
+func (g *gocui.Gui) defaultSettings() {
+	g.InputEsc = true
 	g.FgColor = gocui.ColorWhite
 	g.BgColor = gocui.ColorBlack
 	g.Mouse = true
