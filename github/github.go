2b33a5f2d1d67d799141ef02766a55b2ac86ed58
tnakata <f.seasons017@gmail.com>
2018-09-27 12:25:56 +0900

    Add GitHub client

---
+++ github/github.go
@@ -0,0 +1,37 @@
+package github
+
+import (
+	"context"
+	"errors"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+type Client struct {
+	owner, repo string
+	*github.Client
+}
+
+func NewClient(owner, token string) (*Client, error) {
+	if len(owner) == 0 {
+		return nil, errors.New("missing GitHub Repository owner")
+	}
+	if len(token) == 0 {
+		return nil, errors.New("missing GitHub API token")
+	}
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	return &Client{
+		owner:  owner,
+		repo:   repo,
+		Client: client,
+	}, nil
+}
