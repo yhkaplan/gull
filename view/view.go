cb0bba9fdd37fdd8fe046af12115b04d0f1690ee
yhkaplan <yhkaplan@gmail.com>
2018-09-27 19:57:15 +0900

    Implement right side

--- view/view.go
+++ view/view.go
@@ -18,11 +18,12 @@ func (manager) Layout(*gocui.Gui) error {
 
 // TODO: move this elsewhere
 type DashboardView struct {
-	g         *gocui.Gui
-	categoryV *gocui.View
+	g            *gocui.Gui
+	categoryView *gocui.View
+	listView     *gocui.View
 }
 
-// Initializes GullView
+// Initializes DashboardView
 func New() *DashboardView {
 	v := &DashboardView{}
 	return v
@@ -41,24 +42,26 @@ func (v *DashboardView) layout(g *gocui.Gui) error {
 	if err != nil {
 		return err
 	}
-	// err = v.setListView(g)
-	// if err != nil {
-	// 	return err
-	// }
+	err = v.setListView(g, horizOffset, maxX, maxY)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
 // Setup left side category window
 func (v *DashboardView) setCategoryView(g *gocui.Gui, horizOffset int, maxY int) error {
-	if categoryV, err := g.SetView("Category", 0, 0, horizOffset, maxY); err != nil {
+	if categoryView, err := g.SetView("Category", 0, 0, horizOffset, maxY); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		categoryV.Frame = true
-		categoryV.BgColor = gocui.ColorMagenta //TODO: color here for testing
-		categoryV.FgColor = gocui.ColorCyan
-		v.categoryV = categoryV
+
+		categoryView.Frame = true
+		categoryView.BgColor = gocui.ColorMagenta //TODO: color here for testing
+		categoryView.FgColor = gocui.ColorCyan
+
+		v.categoryView = categoryView
 
 		//TODO: Add go func call to get info here
 	}
@@ -66,10 +69,25 @@ func (v *DashboardView) setCategoryView(g *gocui.Gui, horizOffset int, maxY int)
 	return nil
 }
 
-// // Setup right side list window
-// func (v *GullView) setListView(g *gocui.Gui) error {
+// Setup right side list window
+func (v *DashboardView) setListView(g *gocui.Gui, horizOffset int, maxX int, maxY int) error {
+	//TODO: Set view name to change dynamically to PR, ALL, etc
+	if listView, err := g.SetView("Events", horizOffset, 0, maxX, maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
 
-// }
+		listView.Frame = true
+		listView.BgColor = gocui.ColorGreen
+		listView.FgColor = gocui.ColorYellow
+
+		v.listView = listView
+
+		//TODO: Add go func call to get info here
+	}
+
+	return nil
+}
 
 func (v *DashboardView) Run() {
 	g, err := gocui.NewGui(gocui.Output256)
